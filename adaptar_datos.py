f18f8411f693ad050c88545ec44f3bb6aef5ca7e
jclevanto <167702627+jclevanto@users.noreply.github.com>
2026-08-21 18:10:31 +0000

    validaciones

--- adaptar_datos.py
+++ adaptar_datos.py
@@ -1,6 +1,50 @@
-import sys, os
+import sys, validaciones
 
-sys.argv[1]=txt
+rutas=sys.argv
 
-sys.argv[2]=json
+if validaciones.input(rutas): #Comprobamos que el formato en el que recivimos los datos sea correcto
+    print("Formato de entrada incorrecto por favor use: python adaptar_datos.py datos/observaciones.txt datos/observaciones.json")
+    exit()
 
+txt=rutas[1]
+
+json=rutas[2]
+
+if validaciones.rutatxt(txt):
+    print("La ruta con las observaciones del SMN no existe")
+    exit()
+
+with open(txt,"r") as observaciones:
+    next(observaciones) #Saltamos el encabezado
+    next(observaciones)
+    for medicion in observaciones:
+        mediciones=medicion.split()
+        for i in range(len(mediciones)):
+            if mediciones[i].isalpha():
+                mediciones[i]=" ".join(mediciones[i:])
+                mediciones=mediciones[:i+1]
+                break    #Permitimos nombres de lugares con mas de una palabra
+        print(mediciones)
+        if validaciones.cantidad_datos(mediciones):
+            # guardar que esta medicion no es correcta pq faltan datos (hacer)
+            continue
+        if validaciones.fecha(mediciones[0]):
+            # guardar que esta medicion no es correcta pq el dia esta mal (hacer)
+            continue
+        if validaciones.hora(mediciones[1]):
+            # guardar que esta medicion no es correcta pq la hora esta mal (hacer)
+            continue
+        if validaciones.numeros(mediciones[2:7]):
+            # guardar que esta medicion no es correcta pq alguno no es numero (mejorable) (hacer)
+            continue
+        if validaciones.humedad(mediciones[3]):
+            # guardar que esta medicion no es correcta pq la humedad esta fuera de rango (hacer)
+            continue
+        if validaciones.direccion(mediciones[5]):
+            # guardar que esta medicion no es correcta pq la direccion esta fuera de rango (hacer)
+            continue
+        if validaciones.velocidad(mediciones[6]):
+            # guardar que esta medicion no es correcta pq la direccion esta fuera de rango (hacer)
+            print("mal")
+            continue
+        
---
+++ validaciones.py
@@ -0,0 +1,61 @@
+import os
+
+def rutatxt(ruta):
+    return not os.path.exists(ruta)
+
+def input(rutas):
+    if len(rutas) != 3 or ".txt" not in rutas[1] or ".json" not in rutas[2]:
+        return True
+    return False
+
+def cantidad_datos(mediciones):
+    if len(mediciones) != 8:
+        return True
+    return False
+
+def fecha (num):
+    if len(num)!=8:
+        return True
+    if not num.isnumeric():
+        return True
+    
+    dia = int(num[:2])
+    mes = int(num[2:4])
+    anio = int(num[4:])
+    if 0>dia>31 or 0>mes>12:
+        return True
+    return False
+
+def hora(num):
+    if not num.isnumeric():
+        return True
+    num=int(num)
+    if 0>num>23:
+        return True
+    return False
+
+def numeros(valores):
+    for numero in valores:
+        # Quitamos el '-' inicial y el primer '.' antes de evaluar .isdigit()
+        es_num = numero.lstrip("-").replace(".", "", 1).isdigit()
+        if not es_num:
+            return True         
+    return False
+
+def humedad(num):
+    num = int(num)
+    if not 0<=num<=100:
+        return True
+    return False
+
+def direccion(num):
+    num = int(num)
+    if not 0<=num=<360:
+        return True
+    return False
+
+def velocidad(num):
+    num = int(num)
+    if not 0<=num:
+        return True
+    return False
